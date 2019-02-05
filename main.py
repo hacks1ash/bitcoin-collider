3c319cded7e317a802462aaea6e447fc163f8525
hackslash <miglesworkspace@gmail.com>
2019-02-06 01:26:23 +0400

    Bug Fixes

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ HEX_CHARS = '0123456789ABCDEF'
 PRIVATE_HEX = '0000000000000000000000000000000000000000000000000000000000000001'
 
 if __name__ == '__main__':
+    print('Getting 1000 the richest addresses')
     parse_bitcoin_addresses(100)
     final_bitcoin_list = list()
     text_content = read_text_from_file('bitcoin_addresses')
@@ -14,12 +15,18 @@ if __name__ == '__main__':
         text_content[index] = text_content[index].rstrip('\n')
         final_bitcoin_list.append(text_content[index])
 
+    print('Starting checking addresses')
+
     for n in range(64, 65):
         for xs in itertools.product(HEX_CHARS, repeat=n):
             private_hex = ''.join(xs)
-            public_address = generate_public_address(private_hex)
-            if public_address in final_bitcoin_list:
-                private_wif = generate_private_wif(private_hex)
-                write_text_to_file('cracked', private_hex)
-                write_text_to_file('cracked', private_wif)
-                write_text_to_file('cracked', public_address)
+            if private_hex.count('0') != 64:
+                print(private_hex)
+                public_address = generate_public_address(private_hex)
+                if public_address in final_bitcoin_list:
+                    private_wif = generate_private_wif(private_hex)
+                    write_text_to_file('cracked', private_hex)
+                    write_text_to_file('cracked', private_wif)
+                    write_text_to_file('cracked', public_address)
+    else:
+        print('Whole blockchain checked ^_^')
